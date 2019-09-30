70329d4ec77a49f48ded30679f053315d98e1e4a
Mórász Bálint <balint@morasz.hu>
2019-09-30 07:48:45 +0200

    fix input data

--- combinatorical-auction/Program.fs
+++ combinatorical-auction/Program.fs
@@ -1,4 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
+// Learn more about F# at http://fsharp.org
 
 open System
 
@@ -8,6 +8,7 @@ type Node = {
 }
 
 type Edge = {
+    id: int;
    fromNode : Node;
    toNode : Node;
    capacityPositive : double;
@@ -65,19 +66,49 @@ let nodes = [
 ]
 
 let edges = [
-    { fromNode = nodes.[1]; toNode = nodes.[0]; capacityPositive = 80.0; capacityNegative = 80.0; };
-    { fromNode = nodes.[2]; toNode = nodes.[0]; capacityPositive = 75.0; capacityNegative = 75.0; };
-    { fromNode = nodes.[3]; toNode = nodes.[0]; capacityPositive = 70.0; capacityNegative = 70.0; };
-    { fromNode = nodes.[2]; toNode = nodes.[1]; capacityPositive = 60.0; capacityNegative = 60.0; };
-    { fromNode = nodes.[3]; toNode = nodes.[1]; capacityPositive = 60.0; capacityNegative = 60.0; };
-    { fromNode = nodes.[3]; toNode = nodes.[2]; capacityPositive = 60.0; capacityNegative = 60.0; };
+    { id = 1; fromNode = nodes.[1]; toNode = nodes.[0]; capacityPositive = 80.0; capacityNegative = 80.0; };
+    { id = 2; fromNode = nodes.[2]; toNode = nodes.[0]; capacityPositive = 75.0; capacityNegative = 75.0; };
+    { id = 3; fromNode = nodes.[3]; toNode = nodes.[0]; capacityPositive = 70.0; capacityNegative = 70.0; };
+    { id = 4; fromNode = nodes.[2]; toNode = nodes.[1]; capacityPositive = 60.0; capacityNegative = 60.0; };
+    { id = 5; fromNode = nodes.[3]; toNode = nodes.[1]; capacityPositive = 60.0; capacityNegative = 60.0; };
+    { id = 6; fromNode = nodes.[3]; toNode = nodes.[2]; capacityPositive = 60.0; capacityNegative = 60.0; };
 ]
 
 let players = [
-    { id = 0; node = nodes.[0]  };
-    { id = 1; node = nodes.[1]  };
-    { id = 2; node = nodes.[2]  };
-    { id = 3; node = nodes.[3]  };
+    { id = 0; node = nodes.[0] };
+    { id = 1; node = nodes.[1] };
+    { id = 2; node = nodes.[2] };
+    { id = 3; node = nodes.[3] };
+]
+
+let sources = players |> List.map (fun p -> p.node.production > 0.0)
+let consumers = players |> List.map (fun p -> p.node.production < 0.0)
+
+let edgePrices = [
+    { player = players.[1]; edge = edges.[0]; price = 9.0; }
+    { player = players.[2]; edge = edges.[0]; price = 9.0; }
+    { player = players.[3]; edge = edges.[0]; price = 4.0; }
+    { player = players.[1]; edge = edges.[1]; price = 5.0; }
+    { player = players.[2]; edge = edges.[1]; price = 8.0; }
+    { player = players.[3]; edge = edges.[1]; price = 8.0; }
+    { player = players.[1]; edge = edges.[2]; price = 11.0; }
+    { player = players.[2]; edge = edges.[2]; price = 11.0; }
+    { player = players.[3]; edge = edges.[2]; price = 11.0; }
+    { player = players.[1]; edge = edges.[3]; price = 1.0; }
+    { player = players.[2]; edge = edges.[3]; price = 4.0; }
+    { player = players.[3]; edge = edges.[3]; price = 4.0; }
+    { player = players.[1]; edge = edges.[4]; price = 4.5; }
+    { player = players.[2]; edge = edges.[4]; price = 4.5; }
+    { player = players.[3]; edge = edges.[4]; price = 4.5; }
+    { player = players.[1]; edge = edges.[5]; price = 5.0; }
+    { player = players.[2]; edge = edges.[5]; price = 1.5; }
+    { player = players.[3]; edge = edges.[5]; price = 5.0; }
+]
+
+let prices = [
+    { fromProducer = players.[0]; toConsumer = players.[1]; price = 25.0; };
+    { fromProducer = players.[0]; toConsumer = players.[2]; price = 23.0; };
+    { fromProducer = players.[0]; toConsumer = players.[3]; price = 20.0; };
 ]
 
 let demands = [
@@ -92,10 +123,26 @@ let demands = [
     { player = players.[3]; fromAmount = 80.0; toAmount = 100.0; price = 36.0; }
 ]
 
-let prices = [
-    { fromProducer = players.[0]; toConsumer = players.[1]; price = 25.0; };
-    { fromProducer = players.[0]; toConsumer = players.[2]; price = 23.0; };
-    { fromProducer = players.[0]; toConsumer = players.[3]; price = 20.0; };
+type PartialRoute = {
+    player: Player;
+    edges: (Edge * Direction) list;
+}
+
+// TODO: this should be calculated;
+let partialRoutes = [
+    { player = players.[1]; edges = [ edges.[0], Negative ]; }
+    { player = players.[1]; edges = [ edges.[1], Negative; edges.[3], Positive ]; }
+    { player = players.[1]; edges = [ edges.[2], Negative; edges.[4], Positive ]; }
+    { player = players.[1]; edges = [ edges.[1], Negative; edges.[5], Negative; edges.[4], Positive ]; }
+    { player = players.[1]; edges = [ edges.[2], Negative; edges.[5], Positive; edges.[3], Positive ]; }
+    { player = players.[2]; edges = [ edges.[1], Negative ]; }
+    { player = players.[2]; edges = [ edges.[0], Negative; edges.[3], Negative ]; }
+    { player = players.[2]; edges = [ edges.[2], Negative; edges.[5], Positive ]; }
+    { player = players.[2]; edges = [ edges.[2], Negative; edges.[4], Positive; edges.[3], Negative ]; }
+    { player = players.[2]; edges = [ edges.[2], Negative ]; }
+    { player = players.[2]; edges = [ edges.[0], Negative; edges.[4], Negative ]; }
+    { player = players.[2]; edges = [ edges.[1], Negative; edges.[5], Negative ]; }
+    { player = players.[2]; edges = [ edges.[1], Negative; edges.[3], Positive; edges.[4], Negative ]; }
 ]
 
 type Route = {
